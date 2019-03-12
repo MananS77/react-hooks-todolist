911b361f2838cf1ef5d1f5f303bc56c355f8dc3b
Manan <manan.shah77@gmail.com>
2019-03-12 18:11:47 +0530

    first commit

---
+++ src/TodoForm.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import TextField from "@material-ui/core/TextField";
+
+const TodoForm = ({saveTodo}) => {
+
+    const [value, setValue] = useState('');
+
+    return (
+        <form
+            className="AddBox"
+            onSubmit={event => {
+                event.preventDefault();
+                saveTodo(value);
+                setValue('');
+            }}
+        >
+            <TextField
+                autoFocus
+                variant="outlined"
+                placeholder="Add todo"
+                margin="normal"
+                onChange={event => {
+                    setValue(event.target.value);
+                }}
+                value={value}
+            />
+        </form>
+    );
+};
+
+export default TodoForm;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+import Typography from "@material-ui/core/Typography";
+import "./styles.css";
+import * as serviceWorker from "./serviceWorker";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+
+const App = () => {
+    const [todos, setTodos] = useState([]);
+    return (
+        <div className="App">
+            <Typography component="h1" variant="h2">
+                Todos
+            </Typography>
+            <TodoForm
+                className="AddBox"
+                saveTodo={todoText => {
+                    const trimmedText = todoText.trim();
+                    if (trimmedText.length > 0) {
+                        setTodos([...todos, trimmedText]);
+                    }
+                }}
+            />
+            <TodoList
+                todos={todos}
+                deleteTodo={todoIndex => {
+                    const updatedTodos = todos.filter((_, index) => index !== todoIndex);
+                    setTodos(updatedTodos);
+                }}
+            />
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
